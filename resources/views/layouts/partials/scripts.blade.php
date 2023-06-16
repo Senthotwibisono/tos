d2a49afc83548cb45775026ccfd63b73b6503a8d
Senthotwibisono <109057152+Senthotwibisono@users.noreply.github.com>
2023-06-16 20:48:40 +0700

    Add files via upload

---
+++ resources/views/layouts/partials/scripts.blade.php
@@ -0,0 +1,7 @@
+<script src="{{ asset('dist/assets/js/app.js') }}"></script>
+<script src="{{ asset('dist/assets/js/bootstrap.js') }}"></script>
+
+<!-- <script src="{{ asset('/js/main.js') }}"></script> -->
+
+<!-- custom_js -->
+@yield('custom_js')
