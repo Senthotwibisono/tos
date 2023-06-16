03680a02ad6e2f327df2cb499be963015c13a668
Senthotwibisono <109057152+Senthotwibisono@users.noreply.github.com>
2023-06-16 22:45:11 +0700

    Update authheader.blade.php

--- resources/views/partial/invoice/authheader.blade.php
+++ resources/views/partial/invoice/authheader.blade.php
@@ -1,17 +1,8 @@
 <div class="dropdown">
                                 <a href="#" id="topbarUserDropdown" class="user-dropdown d-flex align-items-center dropend dropdown-toggle " data-bs-toggle="dropdown" aria-expanded="false">
-                                  <div class="user-img d-flex align-items-center">                                            
-                                            @if (Auth::user()->profil)
-                                            <div class="round-image-3">
-                                        <img class="w-100 active" src="{{ asset('profil/' .Auth::user()->profil) }}" data-bs-target="#Gallerycarousel" data-bs-slide-to="0">
-                                        <div class="avatar avatar-md">
-                                    @else
-                                    <div class="avatar avatar-md">
-                                        <img class="w-100 active" src="{{ asset('dist/assets/images/faces/1.jpg') }}" data-bs-target="#Gallerycarousel" data-bs-slide-to="0">
-                                        </div>
-                                    @endif
-                                            
-                                        </div>
+                                    <div class="avatar avatar-md2" >
+                                        <img src="{{asset('dist/assets/images/faces/1.jpg')}}" alt="Avatar">
+                                    </div>
                                     <div class="text">
                                         <h6 class="user-dropdown-name">{{ Auth::user()->name }}</h6>
                                         <p class="user-dropdown-status text-sm text-muted">{{ Auth::user()->email }}</p>
