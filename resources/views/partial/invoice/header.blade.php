25cccada825ddb71abd2fb26fa5b3b47a8adf02d
Senthotwibisono <109057152+Senthotwibisono@users.noreply.github.com>
2023-06-16 22:35:53 +0700

    Update header.blade.php

--- resources/views/partial/invoice/header.blade.php
+++ resources/views/partial/invoice/header.blade.php
@@ -3,8 +3,8 @@
     <title>Layout Horizontal - Mazer Admin Dashboard</title>
     
     <link rel="stylesheet" href="{{asset('dist/assets/css/main/app.css')}}">
-    <link rel="shortcut icon" href="{{asset('dist/assets/images/logo/favicon.svg')}}" type="image/x-icon">
-    <link rel="shortcut icon" href="{{asset('dist/assets/images/logo/favicon.png')}}" type="image/png">
+    <link rel="shortcut icon" href="{{asset('logo/icon.png')}}" type="image/x-icon">
+    <link rel="shortcut icon" href="{{asset('logo/icon.png')}}" type="image/png">
     
 <link rel="stylesheet" href="{{asset('dist/assets/css/shared/iconly.css')}}">
 <link rel="stylesheet" href="{{ asset('fontawesome/css/all.min.css') }}">
