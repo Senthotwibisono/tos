4f6e8a424a7d2bf285f8f1a543d936f844780d51
Senthotwibisono <109057152+Senthotwibisono@users.noreply.github.com>
2023-06-20 16:29:27 +0700

    Update main.blade.php

--- resources/views/partial/auth/main.blade.php
+++ resources/views/partial/auth/main.blade.php
@@ -12,17 +12,19 @@
 <div class="row h-100">
     <div class="col-lg-5 col-12">
         <div id="auth-left">
-            <div style="position: relative; left: -100px;">
-            <img class="logo" src="{{asset('logo/ICON2.png')}}" alt="auth-left">
+            
+        <div style="position: relative; left: -250px;">
+            <img class="logoicon" src="{{asset('logo/ICON2.png')}}" alt="auth-left">
         </div>
+            
             <h1 class="auth-title">Log in.</h1>
             <p class="auth-subtitle mb-5">Log in with your data that you entered during registration.</p>
             @yield('content')
-            <div class="text-center mt-5 text-lg fs-4">
-                <!-- <p class="text-gray-600">Don't have an account? <a href="auth-register.html" class="font-bold">Sign
+            <!-- <div class="text-center mt-5 text-lg fs-4">
+                <p class="text-gray-600">Don't have an account? <a href="auth-register.html" class="font-bold">Sign
                         up</a>.</p>
-                <p><a class="font-bold" href="auth-forgot-password.html">Forgot password?</a>.</p> -->
-            </div>
+                <p><a class="font-bold" href="auth-forgot-password.html">Forgot password?</a>.</p> 
+            </div> -->
         </div>
     </div>
     <div class="col-lg-7 d-none d-lg-block">
