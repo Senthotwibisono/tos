9371f22e176ed1d6a63bb4682befe6219fde3075
fdw22 <dzulfiqar33@gmail.com>
2023-07-05 19:29:52 +0700

    installing dependencies on main branch

---
+++ storage/framework/views/28eaa022a73977b074e206345b79627b.php
@@ -0,0 +1,8 @@
+<div class="footer clearfix mb-0 text-muted">
+    <div class="float-start">
+        <p>2022 &copy; ICON SARANA</p>
+    </div>
+    <div class="float-end">
+        <p>Managed by <a href="https://www.iconline.co.id/">ICON</a></p>
+    </div>
+</div><?php /**PATH E:\Fdw File Storage 1\CTOS\dev\frontend\tos-dev-local\resources\views/partial/footer.blade.php ENDPATH**/ ?>
